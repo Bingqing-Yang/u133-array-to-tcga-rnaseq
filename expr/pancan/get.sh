57c6972adbdcb588dd5b55fd850afec10b9404fa
ncyue2 <ncyue2@connect.hku.hk>
2023-11-21 11:20:11 +0800

    Create directory gdc.

--- expr/pancan/get.sh
+++ expr/pancan/get.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Retrieve raw data
+# modify Dr. David Shih get.sh script
+# credit to Dr. David Shih
 
 set -euo pipefail
 
-# TODO (with help from Noel)
-
-# wget
-# curl
+mkdir gdc
+# "gdc-client" can be replaced by the location of the gdc-client script
+gdc-client download -m manifest/PanCan-General_Open_GDC-Manifest_2.txt -d gdc
